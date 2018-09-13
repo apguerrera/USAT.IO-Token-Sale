b16515eae78ea19e1cc62c4ac224d7cffa22a8a7
Ivan Jasenovic <ijasenov@gmail.com>
2018-09-13 18:16:50 +1000

    update to use openzeppelin v2

--- contracts/USAT.sol
+++ contracts/USAT.sol
@@ -1,25 +1,25 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.24;
 
 // modified from Moritz Neto with BokkyPooBah / Bok Consulting Pty Ltd Au 2017. 
 // The MIT Licence.
 
 // Added Open Zeppelin  
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
-
 
 contract ApproveAndCallFallBack {
     function receiveApproval(address from, uint256 tokens, address token, bytes data) public;
 }
 
-contract USATToken is ERC20, Ownable, SafeMath {
+contract USATToken is ERC20, Ownable {
     string public symbol;
     string public  name;
     uint8 public decimals;
-    uint public _totalSupply;
+    uint256 public _totalSupply;
 
-    mapping(address => uint) balances;
-    mapping(address => mapping(address => uint)) allowed;
+    mapping(address => uint256) balances;
+    mapping(address => mapping(address => uint256)) allowed;
 
     //event USATSenderCheck(address owner, address sender, address spender);
     //event USATAllowed(address owner, address from, address sender, address to, uint token, uint org, uint diff);
@@ -29,55 +29,55 @@ contract USATToken is ERC20, Ownable, SafeMath {
         decimals = 18;
         _totalSupply = 1525000000000000000000000000; //1,525,000,000
         balances[owner] = _totalSupply;
-        Transfer(address(0), owner, _totalSupply);
+        emit Transfer(address(0), owner, _totalSupply);
     }
 
 
-    function totalSupply() public constant returns (uint) {
+    function totalSupply() public constant returns (uint256) {
         return _totalSupply  - balances[address(0)];
     }
 
 
-    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+    function balanceOf(address tokenOwner) public constant returns (uint256 balance) {
         return balances[tokenOwner];
     }
 
 
-    function transfer(address to, uint tokens) public returns (bool success) {
-        balances[msg.sender] = safeSub(balances[msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        Transfer(msg.sender, to, tokens);
+    function transfer(address to, uint256 tokens) public returns (bool success) {
+        balances[msg.sender] = SafeMath.sub(balances[msg.sender], tokens);
+        balances[to] = SafeMath.add(balances[to], tokens);
+        emit Transfer(msg.sender, to, tokens);
         
         return true;
     }
 
 
-    function approve(address spender, uint tokens) public returns (bool success) {
+    function approve(address spender, uint256 tokens) public returns (bool success) {
         //emit USATSenderCheck(owner, msg.sender, spender);
         allowed[msg.sender][spender] = tokens;
-        Approval(msg.sender, spender, tokens);
+        emit Approval(msg.sender, spender, tokens);
         return true;
     }   
 
 
-    function transferFrom(address from, address to, uint tokens) public returns (bool success) {
-        balances[from] = safeSub(balances[from], tokens);
+    function transferFrom(address from, address to, uint256 tokens) public returns (bool success) {
+        balances[from] = SafeMath.sub(balances[from], tokens);
         //emit USATAllowed(owner, from, msg.sender, to, tokens, allowed[from][msg.sender], allowed[from][msg.sender] - tokens);
-        allowed[from][msg.sender] = safeSub(allowed[from][msg.sender], tokens);
-        balances[to] = safeAdd(balances[to], tokens);
-        Transfer(from, to, tokens);
+        allowed[from][msg.sender] = SafeMath.sub(allowed[from][msg.sender], tokens);
+        balances[to] = SafeMath.add(balances[to], tokens);
+        emit Transfer(from, to, tokens);
         return true;
     }
 
 
-    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+    function allowance(address tokenOwner, address spender) public constant returns (uint256 remaining) {
         return allowed[tokenOwner][spender];
     }
 
 
-    function approveAndCall(address spender, uint tokens, bytes data) public returns (bool success) {
+    function approveAndCall(address spender, uint256 tokens, bytes data) public returns (bool success) {
         allowed[msg.sender][spender] = tokens;
-        Approval(msg.sender, spender, tokens);
+        emit Approval(msg.sender, spender, tokens);
         ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, this, data);
         return true;
     }
@@ -87,8 +87,7 @@ contract USATToken is ERC20, Ownable, SafeMath {
         revert();
     }
 
-
-    function transferAnyERC20Token(address tokenAddress, uint tokens) public onlyOwner returns (bool success) {
-        return ERC20Interface(tokenAddress).transfer(owner, tokens);
+    function transferAnyERC20Token(address tokenAddress, uint256 tokens) public onlyOwner returns (bool success) {
+        return IERC20(tokenAddress).transfer(owner, tokens);
     }
 }
--- truffle.js
+++ truffle.js
@@ -1,4 +1,4 @@
-var HDWalletProvider = require("truffle-hdwallet-provider");
+//var HDWalletProvider = require("truffle-hdwallet-provider");
 var mnemonic = "hazard woman alert member blossom situate picture average stumble creek swim barrel space gas say";
 
 module.exports = {
@@ -12,14 +12,14 @@ module.exports = {
 			host: "localhost",
 			port: 9545,
 			network_id: "*"
-		},
-		rinkeby: {
+		}
+	/*	,rinkeby: {
 			provider: function() {
 				return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/AmbDCnH7o4e8izb0VyxN", 0, 10)
 			},
 			network_id: 5,
 			gas: 7099718,
 			gasPrice: 100000000000
-		}
+		}*/
 	}
 };
