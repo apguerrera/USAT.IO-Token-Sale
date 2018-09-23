a0b1cb45231a365401385916a3ba68dc46345f72
Ivan Jasenovic <1901097+Ivantt@users.noreply.github.com>
2018-09-23 22:54:10 +1000

    Path to Zeppelin
    Updated to zeppelin dependancies

--- truffle.js
+++ truffle.js
@@ -2,12 +2,6 @@
 var mnemonic = "hazard woman alert member blossom situate picture average stumble creek swim barrel space gas say";
 
 module.exports = {
-	solc: {
-       optimizer: {
-          enabled: true,
-          runs: 200
-        }
-    }
 	networks: {
 		development: {
 			host: "localhost",
@@ -19,13 +13,13 @@ module.exports = {
 			port: 9545,
 			network_id: "*"
 		}
-		,rinkeby: {
+	/*	,rinkeby: {
 			provider: function() {
 				return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/AmbDCnH7o4e8izb0VyxN", 0, 10)
 			},
 			network_id: 5,
 			gas: 7099718,
 			gasPrice: 100000000000
-		}
+		}*/
 	}
 };
