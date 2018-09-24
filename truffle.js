3353f995c22ac68db385dde44863a14384bb0d0a
Ivan Jasenovic <1901097+Ivantt@users.noreply.github.com>
2018-09-24 21:59:35 +1000

    USATTokenJason
    Moved to root

--- truffle.js
+++ truffle.js
@@ -13,13 +13,14 @@ module.exports = {
 			port: 9545,
 			network_id: "*"
 		}
-	/*	,rinkeby: {
+		,rinkeby: {
 			provider: function() {
 				return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/AmbDCnH7o4e8izb0VyxN", 0, 10)
 			},
 			network_id: 5,
 			gas: 7099718,
 			gasPrice: 100000000000
-		}*/
+		}
 	}
+
 };
